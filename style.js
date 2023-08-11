629529359f0065852cf6fadd401621ab7ddd7949
Ricky Vang <vangcheemeng@yahoo.com>
2023-08-11 15:23:20 -0700

    if else / switch

--- style.js
+++ style.js
@@ -10,3 +10,63 @@
     > less than
     == equal 
 */
+
+// IF ELSE STATEMENTS
+
+var result = 50;
+
+if (result > 40) {
+    console.log("Congrats! You passed the test!")
+} else {
+    console.log("Sorry, you did not pass the test!")
+}
+
+//IF ELSE STATEMETNS
+
+var place = "first"
+
+if (place == "first") {
+    console.log("GOLD")
+} else if (place == "second") {
+    console.log("SILVER")
+} else if (place == "third") {
+    console.log("BRONZE")
+} else {
+    console.log("NO MEDAL")
+}
+
+// SWITCH STATEMENTS
+
+var place = "second"
+
+switch (place) {
+    case "first":
+        console.log("GOLD")
+        break;
+    case "second":
+        console.log("SILVER")
+        break;
+    case "third":
+        console.log("BRONZE")
+        break;
+    default:
+        console.log("NO MEDAL")
+}
+
+var light = "green"
+
+switch(light) {
+    case 'green':
+        console.log("Drive");
+        break;
+    case 'orange':
+        console.log("Get ready");
+        break;
+    case 'red':
+        console.log("Don't drive");
+        break;
+    default:
+        //this block will run if no condition matches
+        console.log('The light is not green, orange, or red');
+        break;
+ }
