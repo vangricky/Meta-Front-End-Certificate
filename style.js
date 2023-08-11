1793aa58d4cfe32cc22c223e420156572b566185
Ricky Vang <vangcheemeng@yahoo.com>
2023-08-11 14:31:49 -0700

    style.js

---
+++ style.js
@@ -0,0 +1,12 @@
+/* Arithmetic Operators in JS
+    + addition 
+    - subtraction 
+    * multiplication
+    / division
+*/ 
+
+/* Comparison operators in JS
+    > greater than
+    > less than
+    == equal 
+*/
