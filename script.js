bd6fefbfd7d0aade6c508946784133ffd3af1a18
Ricky Vang <vangcheemeng@yahoo.com>
2023-08-12 16:04:48 -0700

    classes

--- script.js
+++ script.js
@@ -333,13 +333,166 @@
 // -------------------------------------------------------------------------------------
 
 // clases
-class car {
-    constructor(color, speed) {
-        this.color = color;
-        this.speed = speed;
-    }
+// class Car {
+//     constructor(color, speed) {
+//         this.color = color;
+//         this.speed = speed;
+//     }
 
-    turboOn() {
-        console.log("Turbo is on!")
-    }
-}
+//     turboOn() {
+//         console.log("Turbo is on!")
+//     }
+// }
+
+// const car1 = new Car("red", 120)
+// car1.turboOn();
+
+// The four fundamental OOP principles are ' inheritance, encapsulation, abstraction, and polymorphism.
+// use the ' new ' keyword to create a new oject.
+// use the ' extends ' keyword to relate an object to another object
+
+// OOP Principle: Polymorphism example:
+// const bicycle = {
+//     bell: function() {
+//         return "Ring, ring! Watch out, please!"
+//     }
+// }
+
+// const door = {
+//     bell: function() {
+//         return "Ring, ring! Come here, please!"
+//     }
+// }
+
+// function ringTheBell(thing) {
+//     console.log(thing.bell())
+// }
+
+// ringTheBell(bicycle)
+
+// below is an example of polymorphism using classes in JavaScript
+
+// class Bird {
+//     useWings() {
+//         console.log("Flying!")
+//     }
+// }
+// class Eagle extends Bird {
+//     useWings() {
+//         super.useWings()
+//         console.log("Barely flapping!")
+//     }
+// }
+// class Penguin extends Bird {
+//     useWings() {
+//         console.log("Diving!")
+//     }
+// }
+// var baldEagle = new Eagle();
+// var kingPenguin = new Penguin();
+// baldEagle.useWings(); // "Flying! Barely flapping!"
+// kingPenguin.useWings(); // "Diving!"
+
+
+// function Icecream(flavor) {
+//     this.flavor = flavor;
+//     this.meltIt = function() {
+//         console.log(`The ${this.flavor} icecream has melted`);
+//     }
+// }
+
+// let kiwiIcecream = new Icecream("kiwi");
+// let appleIcecream = new Icecream("apple");
+// kiwiIcecream; // --> Icecream {flavor: 'kiwi', meltIt: ƒ}
+// appleIcecream; // --> Icecream {flavor: 'apple', meltIt: ƒ}
+
+// prototype
+
+// var bird = {
+//     hasWings: true,
+//     canFly: true,
+//     hasFeathers: true 
+// }
+//  var eagle1 = Object.create(bird);
+//  console.log("eagle1: ", eagle1);
+
+//  console.log("eagle1 has wings:", eagle1.hasWings);
+//  console.log("eagle1 can fly:", eagle1.canFly);
+//  console.log("eagle1 has feathers:", eagle1.hasFeathers)
+
+//  var eagle2 = Object.create(bird);
+//  console.log("eagle2 has wings:", eagle2.hasWings);
+
+//  var penguin1 = Object.create(bird);
+//  penguin1.canFly = false;
+//  console.log("penguin1: ",penguin1);
+//  console.log("penguin1 has feathers:", penguin1.hasFeathers);
+//  console.log("penguin1 can fly:", penguin1.canFly);
+
+//  class Train {
+//     constructor(color, lightsOn) {
+//         this.color = color;
+//         this.lightsOn = lightsOn;
+//     }
+//     toggleLights() {
+//         this.lightsOn = !this.lightsOn;
+//     }
+//     lightsStatus() {
+//         console.log("Lights on?", this.lightsOn);
+//     }
+//     getSelf() {
+//         console.log(this);
+//     } 
+//     getPrototype() {
+//         var proto = Object.getPrototypeOf(this);
+//         console.log(proto);
+//     }
+//  }
+
+// var myFirstTrain = new Train("red", false);
+// console.log(myFirstTrain)
+
+// var mySecondTrain = new Train("blue", false);
+// var myThirdTrain = new Train("blue", false);
+
+// var train4 = new Train('red', false);
+// train4.toggleLights();
+// train4.lightsStatus();
+// train4.getSelf();
+// train4.getPrototype();
+
+// class HighSpeedTrain extends Train {
+//     constructor(passengers, highSpeedOn, color, lightsOn) {
+//         super(color, lightsOn);
+//         this.passengers = passengers;
+//         this.highSpeedOn = highSpeedOn;
+//     }
+//     toggleHighSpeed() {
+//         this.highSpeedOn = !this.highSpeedOn;
+//         console.log("High speed status:", this.highSpeedOn);
+//     }
+//     toggleLights() {
+//         super.toggleLights();
+//         super.lightsStatus();
+//         console.log("Lights are 100% operation.")
+//     }
+// }
+
+// var myFirstTrain = new Train('red', false);
+// console.log(myFirstTrain); // Train {color: 'red', lightsOn: false}
+// var mySecondTrain = new Train('blue', false);
+// var myThirdTrain = new Train('blue', false);
+
+// var train4 = new Train('red', false);
+// train4.toggleLights(); // undefined
+// train4.lightsStatus(); // Lights on? true
+// train4.getSelf(); // Train {color: 'red', lightsOn: true}
+// train4.getPrototype(); // {constructor: f, toggleLights: f, ligthsStatus: f, getSelf: f, getPrototype: f}
+
+// var train5 = new Train('blue', false);
+// var highSpeed1 = new HighSpeedTrain(200, false, 'green', false);
+
+// train5.toggleLights(); // undefined
+// train5.lightsStatus(); // Lights on? true
+// highSpeed1.toggleLights(); // Lights on? true, Lights are 100% operational.
+
