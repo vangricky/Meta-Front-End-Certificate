51a575ae89bfa4f7dd80338321c54de0df4fea2d
Ricky Vang <vangcheemeng@yahoo.com>
2023-08-23 17:35:42 -0700

    react

---
+++ customizing-example/src/components/Intro3.js
@@ -0,0 +1,13 @@
+function Intro3() {
+    return (
+        <nav className="blog-post-intro">
+            <h2>What's the best way to style your React apps?</h2>
+            <div>
+                <p>There are so many options to choose from. Here's a high-level overview of the popular ones.</p>
+                <p className="link">Read more...</p>
+            </div>
+        </nav>
+    );
+};
+
+export default Intro3;
